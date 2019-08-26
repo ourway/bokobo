f183df303d6f5134a7dfa5f83011ddbf49a42ee6
Farsheed Ashouri <rodmena@@me.com>
2019-08-27 03:10:25 +0430

    Closes #69

--- configs.py
+++ configs.py
@@ -22,7 +22,7 @@ CELERY_DATABASE_URI = 'db+postgresql+psycopg2://{}:{}@{}:{}/{}'.format(CeleryCon
                                                                                 CeleryConfigs.db_host, CeleryConfigs.db_port,
                                                                                 CeleryConfigs.db_name)
 
-ADMINISTRATORS = ['admin']
+ADMINISTRATORS = ['admin','kk']
 SIGNUP_USER = 'signup_user'
 
 
