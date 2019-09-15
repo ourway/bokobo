f41e8f49adce4d53bbdb0e9b133dd32af0338ba8
Farsheed Ashouri <rodmena@@me.com>
2019-09-15 11:42:56 +0430

    closes #88

--- book_collections/urls.py
+++ book_collections/urls.py
@@ -12,7 +12,7 @@ def call_router(app):
               delete_books_from_collection,
               apply=[check_auth, inject_db, pass_data])
     app.route('/collections/collection', 'DELETE', delete_collection,
-              apply=[check_auth, inject_db])
+              apply=[check_auth, inject_db,pass_data])
     app.route('/collections', 'POST', add, apply=data_plus_wrappers)
     app.route('/collections/book', 'POST', add_book_to_collections,
               apply=data_plus_wrappers)
