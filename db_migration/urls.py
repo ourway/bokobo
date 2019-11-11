0f95707f4066bffbf4e8e50fd061cc45b3632529
Farsheed Ashouri <rodmena@@me.com>
2019-11-11 10:22:08 +0330

    closes #103

--- db_migration/urls.py
+++ db_migration/urls.py
@@ -1,5 +1,6 @@
 from helper import check_auth, inject_db, jsonify, pass_data
 from .book import book_press_settling
+from .permissions import permissions_to_db
 
 
 def call_router(app):
@@ -9,3 +10,5 @@ def call_router(app):
     data_plus_wrappers.append(pass_data)
 
     app.route('/book-press-settle', 'GET', book_press_settling, apply=wrappers)
+    app.route('/permissions/inject-db', 'POST',permissions_to_db , apply=wrappers)
+
