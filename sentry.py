23a09be64ad14b67e5408273620de333f768506e
Farsheed Ashouri <rodmena@@me.com>
2019-11-12 12:45:53 +0330

    call_back_url changed

--- sentry.py
+++ sentry.py
@@ -1,6 +1,7 @@
 from sentry_sdk import init, capture_message
 
-init("https://mydsn@sentry.io/123")
+init("https://d8e7b626ed4c46b388351e3b62ce4ced@sentry.io/1816701")
+
 
 capture_message("Hello World")  # Will create an event.
 
