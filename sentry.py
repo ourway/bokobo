763164b304f4643461f2a4c5fabfa8d591764d02
Farsheed Ashouri <rodmena@@me.com>
2019-11-13 16:49:58 +0330

    sentry config changed3

--- sentry.py
+++ sentry.py
@@ -1,8 +1,12 @@
+from raven.base import Raven
 from sentry_sdk import init, capture_message, capture_exception
 from sentry_sdk.integrations.bottle import BottleIntegration
 
-sentry_client = init("https://d8e7b626ed4c46b388351e3b62ce4ced@sentry.io/1816701",
+sentry_client = init(dsn="https://d8e7b626ed4c46b388351e3b62ce4ced@sentry.io/1816701",
     integrations=[BottleIntegration()])
+
+
+
 
 
 capture_message("Hello World")  # Will create an event.
